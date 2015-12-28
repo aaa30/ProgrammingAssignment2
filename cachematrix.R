cb37d18ae719476633849584da6bd3021b7ce27c
unknown <aaa30@duke.edu>
2015-12-27 20:37:01 -0500

    Add cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -6,13 +6,13 @@
 
 ## Write a short comment describing this function
 # 
-# #The first function, `makeVector` creates a special "vector", which is
+# #The first function, `makeCacheMatrix` creates a special "matrix" that can cache its inverse, which is
 # really a list containing a function to
 # 
-# 1.  set the value of the vector
-# 2.  get the value of the vector
-# 3.  set the value of the mean
-# 4.  get the value of the mean
+# 1.  set the value of the matrix
+# 2.  get the value of the matrix
+# 3.  set the value of the matrix' inverse
+# 4.  get the value of the matrix' inverse
 
 makeCacheMatrix <- function(x = matrix()) {
   matinv <- NULL
@@ -28,6 +28,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+# 
+#  The second function, "Cachesolve" computes the inverse of the special
+# "matrix" returned by `makeCacheMatrix` above. If the inverse has
+# already been calculated (and the matrix has not changed), then
+# `cacheSolve` retrieves the inverse from the cache. The solve function computes the inverse of a square matrix
 
 cacheSolve <- function(x, ...) {
   matinv <- x$getinverse()
